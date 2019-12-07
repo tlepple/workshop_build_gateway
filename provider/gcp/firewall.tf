6bdcccd02d36644ef00cf35414379359d241b9b6
root <rtlepple@gmail.com>
2019-12-07 16:49:20 +0000

    initial commit

--- provider/gcp/firewall.tf
+++ provider/gcp/firewall.tf
@@ -7,9 +7,6 @@ resource "google_compute_firewall" "allow-ssh" {
         protocol = "tcp"
         ports    = ["22"]
     }
-#    source_ranges = [ "72.47.39.106/32" ]
-#    source_ranges = ["0.0.0.0/0"]
-#    source_ranges = [ "${var.my_public_ip}" ]
     source_ranges = [ "${var.mypublicip_cidr}" ]
 }
 
