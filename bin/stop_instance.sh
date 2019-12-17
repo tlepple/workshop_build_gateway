e87dafd12b92aedeb32d596b1bf02a5c44b85ebc
root <rtlepple@gmail.com>
2019-12-17 15:14:33 +0000

    initial commit

---
+++ bin/stop_instance.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#########################################################
+# Input parameters
+#########################################################
+
+case "$1" in
+        aws)
+           echo "you chose aws"
+            ;;
+        azure)
+           echo "you chose azure"
+            ;;
+        gcp)
+	   echo "you chose gcp"
+            ;;
+        *)
+            echo $"Usage: $0 {aws|azure|gcp}"
+            echo $"example: ./stop_instance.sh azure"
+            echo $"example: ./stop_instance.sh.sh aws"
+            echo $"example: ./stop_instance.sh gcp"
+#            exit 0
+esac
+
+CLOUD_PROVIDER=$1
+
+########################################################
+# utility functions
+#########################################################
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+starting_dir=`pwd`
+
+
+#####################################################
+# Execute cloud provider specific code
+#####################################################
+
+case "$CLOUD_PROVIDER" in
+        aws)
+            echo "execute aws code here... "
+	  #  . /app/workshop_build_gateway/provider/aws/aws_start_instance.sh
+            ;;
+        azure)
+           echo "execute azure code here"
+          #  . /app/workshop_build_gateway/provider/azure/azure_start_instance.sh
+            ;;
+        gcp)
+           echo "execute gcp code here..."
+#             echo `pwd`
+#             cd ./provider/gcp
+             . /app/workshop_build_gateway/provider/gcp/gcp_stop_instance.sh
+            ;;
+        *)
+            echo "you had a different choice... is this block needed?"
+	    ;;
+esac
+
+cd $starting_dir
--- provider/gcp/gcp_stop_instance.sh
+++ provider/gcp/gcp_stop_instance.sh
@@ -7,16 +7,14 @@
 . $starting_dir/provider/gcp/.env.template
 
 
+
 RUN_STATE=`gcloud compute instances describe $LV_VM_MASTER_NAME --zone=$LV_ZONE --project $LV_PROJECT_NAME --format="json"| jq -r ".status"`
 
 echo "Current run state is: " ${RUN_STATE}
 
-if [ ${RUN_STATE} == "TERMINATED" ]; then
-    gcloud compute instances start ${LV_VM_MASTER_NAME} --zone=${LV_ZONE} --project ${LV_PROJECT_NAME}
-    #  call the connection strings script:
+if [ ${RUN_STATE} == "RUNNING" ]; then
+    gcloud compute instances stop ${LV_VM_MASTER_NAME} --zone=${LV_ZONE} --project ${LV_PROJECT_NAME}
     echo
-    echo
-    . ${starting_dir}/provider/gcp/echo_conn_strings.sh
 else
     echo "Instance " ${LV_VM_MASTER_NAME} " is in a different state: " ${RUN_STATE}
 fi
